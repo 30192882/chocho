9475b8979e26d1b7dc2f37aabb6a1bf98377fa75
30192882 <90853294+30192882@users.noreply.github.com>
2024-08-12 13:18:24 +0900

    0812 폴바셋 메뉴

--- paul/js/main.js
+++ paul/js/main.js
@@ -84,4 +84,63 @@ $(document).ready(function(){
         $('header').removeClass('menu_over')
         $('header .header_sub .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
     })
+
+
+    /*
+    모바일 메뉴
+    header .header_sub .gnb .gnb_wrap .depth1 > li > a 를 클릭했을 때
+    1차메뉴 a의 href값을 무력화 (즉, 클릭해도 해당 페이지로 이동되지 않도록)
+    li에 open 클래스를 줘야함
+    열려있는 메뉴를 클릭하면 닫히고, 닫힌 메뉴를 클릭하면 열림
+    (동시에 여러개의 메뉴가 열릴 수 있음)
+    */
+    $('header .header_sub .gnb .gnb_wrap .depth1 > li > a').on('click', function(e){
+        if(pc_mobile == 'mo'){ //모바일에서만 작동
+            e.preventDefault(); /* a 태그의 href를 작동시키지 않음 */
+            $(this).parent().toggleClass('open')
+        }
+    })
+
+    $('header .header_sub .gnb .gnb_open').on('click', function(){
+        $('header').addClass('menu_open')//하단 컨텐츠 스크롤 금지
+        $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
+    })
+    $('header .header_sub .gnb .gnb_close').on('click', function(){
+        $('header').removeClass('menu_open')//하단 컨텐츠 스크롤 금지 해제
+        $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
+    })
+
+    $('.menu .list .popup').slick({
+        dots: false, //하단 페이지 버튼 (true, false)
+        arrows: false,  //다음, 이전팝업 (true, false)
+        slidesToShow: 6, //한번에 보일 팝업 수
+        swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+        /*centerMode: true, //가운데정렬(가운데가 1번)*/
+        responsive: [
+            {
+              breakpoint: 1300, //1300px 이하
+              settings: {
+                slidesToShow: 4
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 375,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+        ]
+    })
 })//$(document).ready
