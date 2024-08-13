40868c14641dd72598ce1183a86bf08835c70613
30192882 <90853294+30192882@users.noreply.github.com>
2024-08-13 13:17:27 +0900

    0813 폴바셋 이벤트

--- paul/js/main.js
+++ paul/js/main.js
@@ -143,4 +143,70 @@ $(document).ready(function(){
             },
         ]
     })
+
+    $('.product .list .popup').slick({
+        slidesToShow: 4, //한번에 보일 팝업 수
+        dots: false, //하단 페이지 버튼 (true, false)
+	    arrows: false,  //다음, 이전팝업 (true, false)
+        swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+        responsive: [
+            {
+              breakpoint: 1300, //1300px 이하
+              settings: {
+                slidesToShow: 4
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 375,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+        ]
+    })
+
+    $('.event .list .popup').slick({
+        slidesToShow: 5, //한번에 보일 팝업 수
+        dots: false, //하단 페이지 버튼 (true, false)
+	    arrows: false,  //다음, 이전팝업 (true, false)
+        swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+        responsive: [
+            {
+              breakpoint: 1300, //1300px 이하
+              settings: {
+                slidesToShow: 5
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 375,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+        ]
+    })
 })//$(document).ready
