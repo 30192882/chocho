2180a61e0f6c384b769b496e1cd4ec97360ad230
30192882 <90853294+30192882@users.noreply.github.com>
2024-08-29 18:00:48 +0900

    0829 천안예술의전당 footer

--- c_art/js/common.js
+++ c_art/js/common.js
@@ -11,21 +11,5 @@ $(document).ready(function(){
         브라우저의 스크롤을 조금이라도 내리면 header에 fixed 클래스 추가
         다시 맨 꼭대기로 이동하면 fixed 클래스 삭제
     */
-
-    let scrolling
-
-    function scroll_chk(){
-        scrolling = $(window).scrollTop()
-        console.log(scrolling)
-        if(scrolling > 50){ //0보다 크다면 - 조금이라도 스크롤
-            $('header').addClass('fixed')
-        }else{//0이거나 0보다 작을때
-            $('header').removeClass('fixed')
-        }
-    }
-
-    scroll_chk()//로딩되었을 때 한번
-    $(window).scroll(function(){ //스크롤할 때마다 1번 실행
-        scroll_chk()
-    })
+   
 })//$(document).ready
